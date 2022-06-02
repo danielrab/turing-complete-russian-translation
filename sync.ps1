b8e4ddac590f2ca688933092c9811581c123b04c
danielrab.work <danielrab.work@gmail.com>
2022-06-03 00:02:18 +0300

    Automatic Sync

--- sync.ps1
+++ sync.ps1
@@ -3,5 +3,7 @@ python .\upload_file.py
 Invoke-WebRequest https://turingcomplete.game/download_translation/8 -OutFile Russian.txt
 Copy-Item Russian.txt "C:\Program Files (x86)\Steam\steamapps\common\Turing Complete\translations\Russian.txt"
 git add .
-git commit -m "Sync with master file"
+git commit -m "Automatic Sync"
 git push
+taskkill /im "Turing Complete.exe"
+Start-Process "C:\Users\s17b1\Desktop\Turing Complete.url"
