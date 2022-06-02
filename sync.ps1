be43a51b1827e44b09a8a63f229fc747e5b06c44
danielrab.work <danielrab.work@gmail.com>
2022-06-02 22:00:54 +0300

    add auto commit to sync.ps1

--- sync.ps1
+++ sync.ps1
@@ -1,2 +1,4 @@
 Invoke-WebRequest https://turingcomplete.game/download_translation/8 -OutFile Russian.txt
 Copy-Item Russian.txt "C:\Program Files (x86)\Steam\steamapps\common\Turing Complete\translations\Russian.txt"
+git add .
+git commit -m "Sync with master file"
