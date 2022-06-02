ec53bd147347495933fbdb4b18e6da980caec832
danielrab.work <danielrab.work@gmail.com>
2022-06-02 23:24:29 +0300

    add pulling to sync.ps1

--- sync.ps1
+++ sync.ps1
@@ -1,3 +1,4 @@
+git pull
 python .\upload_file.py
 Invoke-WebRequest https://turingcomplete.game/download_translation/8 -OutFile Russian.txt
 Copy-Item Russian.txt "C:\Program Files (x86)\Steam\steamapps\common\Turing Complete\translations\Russian.txt"
