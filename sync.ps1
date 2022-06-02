510548b2a2d7407ce825b31d4104cd550295e829
danielrab.work <danielrab.work@gmail.com>
2022-06-02 22:01:33 +0300

    add git push to sych.ps1

--- sync.ps1
+++ sync.ps1
@@ -2,3 +2,4 @@ Invoke-WebRequest https://turingcomplete.game/download_translation/8 -OutFile Ru
 Copy-Item Russian.txt "C:\Program Files (x86)\Steam\steamapps\common\Turing Complete\translations\Russian.txt"
 git add .
 git commit -m "Sync with master file"
+git push
