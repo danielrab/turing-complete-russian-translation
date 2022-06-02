5e49f8401d671d8d156d053f27ad45007fefbd9b
danielrab.work <danielrab.work@gmail.com>
2022-06-02 21:54:40 +0300

    add sync

---
+++ sync.ps1
@@ -0,0 +1,2 @@
+Invoke-WebRequest https://turingcomplete.game/download_translation/8 -OutFile Russian.txt
+cp Russian.txt "C:\Program Files (x86)\Steam\steamapps\common\Turing Complete\translations\Russian.txt"
