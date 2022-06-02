6814a49039f12e15b18e02b1f95a5ae797112602
danielrab.work <danielrab.work@gmail.com>
2022-06-02 23:20:59 +0300

    update upload_file.py

--- upload_file.py
+++ upload_file.py
@@ -1,11 +1,13 @@
 import requests
 import os
-import re
+import sqlite3
 
 with open('path_to_progress_ftile.txt') as file:
   save_path = file.read().strip()
-with open(os.path.join(save_path, 'progress.dat'), 'rb') as file:
-  progress_data = file.read()
-token = re.search(b'account_token([0-9A-F]{40})', progress_data).group(1).decode('ascii')
+database = sqlite3.connect(os.path.join(save_path, 'progress.dat'))
+cursor = database.execute("SELECT value FROM progress WHERE key='account_token'")
+token ,= cursor.fetchone()
+cursor.close()
+database.close()
 r = requests.post('https://turingcomplete.game/translate', data={"language_id": 8}, files={"translation": open('Russian.txt', 'rb')}, cookies={"token":token})
 print(r)
