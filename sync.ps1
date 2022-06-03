7bce3cc61f673fa3f6d1733128708614e6f5ec9d
danielrab.work <danielrab.work@gmail.com>
2022-06-04 00:00:15 +0300

    Automatic Sync

--- sync.ps1
+++ sync.ps1
@@ -3,6 +3,7 @@ python .\upload_file.py
 Invoke-WebRequest https://turingcomplete.game/download_translation/8 -OutFile Russian.txt
 Invoke-WebRequest https://turingcomplete.game/download_translation_missing/8 -OutFile Russian_missing.txt
 Copy-Item Russian.txt "C:\Program Files (x86)\Steam\steamapps\common\Turing Complete\translations\Russian.txt"
+Copy-Item "C:\Program Files (x86)\Steam\steamapps\common\Turing Complete\translations\English.txt" English.txt
 git add .
 git commit -m "Automatic Sync"
 git push
